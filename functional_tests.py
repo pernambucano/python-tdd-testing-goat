6b536db9c38657fb4011454723b01812c07bd514
Paulo <paulo@Paulos-MacBook-Pro.local>
2018-03-06 15:36:45 -0300

    Extended functional test to include fake user actions and feedback from the website.

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -11,19 +12,35 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
 
+        # User opens website
         self.browser.get('http://localhost:8000')
+
+        # User notices the title contains 'To-Do'
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test')
 
+        # User notices the header contains 'To-Do'
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)  
 
-
-        # User invited to use the To-DO
+        # User is invited to use the To-DO
+        input_text = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(input_text.get_attribute('placeholder'), 'Please enter a To-Do')
 
         # User types "It's on"
+        input_text.send_keys('It\'s on')
 
         # User hits enter, and "It's on" appears on the screen
+        input_text.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        assertTrue(
+            any(row.text == '1: It\'s on' for row in rows)
+        )
 
         # User enters new text, "Oh Yeah"
+        self.fail('Finish the test!')
 
         # Page updated showing both texts
 
