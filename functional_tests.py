5c5743035c8e422ea87dbeebbc12274ef9aa3593
Paulo <paulo@Paulos-MacBook-Pro.local>
2018-03-07 00:04:16 -0300

    improved functional test

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_text_in_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])  
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         # User opens website
@@ -32,20 +37,20 @@ class NewVisitorTest(unittest.TestCase):
 
         # User hits enter, and "It's on" appears on the screen
         input_text.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(1) 
+        
+        self.check_for_text_in_table('1: It\'s on')
 
         # User enters new text, "Oh Yeah"
         input_text = self.browser.find_element_by_id('id_new_item')
         input_text.send_keys('Oh Yeah')
         input_text.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(1) 
 
+        self.check_for_text_in_table('1: It\'s on')
+        self.check_for_text_in_table('2: Oh Yeah')
 
-        
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: It\'s on', [row.text for row in rows]) 
-        self.assertIn('2: Oh Yeah', [row.text for row in rows])
+ 
 
 
         
