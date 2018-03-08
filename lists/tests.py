a90fb87c7dda43d90e1f19938442913155d0851d
Paulo <paulo@Paulos-MacBook-Pro.local>
2018-03-07 23:48:41 -0300

    Add migrations and first model

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from lists.models import Item
 
 class HomePageTest(TestCase): 
     
@@ -14,3 +15,20 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text':'New item'})
         self.assertIn('New item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+class ItemModelTest(TestCase):
+    
+    def test_saving_and_retrieving_item(self):
+        first_item = Item()
+        first_item.text = "First item"
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "Second Item"
+        second_item.save()
+
+        saved_objects = Item.objects.all()
+        self.assertEqual(saved_objects.count(), 2)
+
+        self.assertEqual(saved_objects[0].text, "First item")
+        self.assertEqual(saved_objects[1].text, "Second Item")
