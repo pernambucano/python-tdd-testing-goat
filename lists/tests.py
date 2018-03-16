6f366034e06ed9b7547dadf6c5f69dae1f15b5f7
Paulo <paulo@Paulos-MacBook-Pro.local>
2018-03-15 23:12:13 -0300

    Add new list url and cleanup tests

--- lists/tests.py
+++ lists/tests.py
@@ -9,26 +9,7 @@ class HomePageTest(TestCase):
     
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')  
-        self.assertTemplateUsed(response, 'home.html')
-    
-    def test_can_save_a_POST_request(self):
-        self.client.post('/', data={'item_text':'New item'})
- 
-        self.assertEqual(Item.objects.count(), 1)
-        item = Item.objects.first()
-        self.assertEqual(item.text, "New item")
-
-    def test_redirect_after_post(self):
-        response = self.client.post('/', data={'item_text':'New item'})
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world')
-
-    def test_only_save_when_necessary(self):
-        response = self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
-
-    
+        self.assertTemplateUsed(response, 'home.html') 
 
 class ItemModelTest(TestCase):
     
@@ -45,7 +26,7 @@ class ItemModelTest(TestCase):
         self.assertEqual(saved_objects.count(), 2)
 
         self.assertEqual(saved_objects[0].text, "First item")
-        self.assertEqual(saved_objects[1].text, "Second Item")
+        self.assertEqual(saved_objects[1].text, "Second Item") 
 
 class ListViewTest(TestCase):
 
@@ -61,3 +42,17 @@ class ListViewTest(TestCase):
     def test_uses_list_template(self):
         response = self.client.get('/lists/the-only-list-in-the-world/') 
         self.assertTemplateUsed(response, 'lists.html' )
+
+class NewListTest(TestCase):
+
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new', data={'item_text':'New item'})
+ 
+        self.assertEqual(Item.objects.count(), 1)
+        item = Item.objects.first()
+        self.assertEqual(item.text, "New item")
+
+    def test_redirect_after_post(self):
+        response = self.client.post('/lists/new', data={'item_text':'New item'})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+
