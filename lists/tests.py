d33df2b40b0148998406592cc65da3d296c31f06
Paulo <paulo@Paulos-MacBook-Pro.local>
2018-03-15 23:41:11 -0300

    Add lists, but a new one for every item

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest, HttpResponse
 from django.template.loader import render_to_string
-from lists.models import Item
+from lists.models import Item, List
 
 class HomePageTest(TestCase): 
     
@@ -11,28 +11,39 @@ class HomePageTest(TestCase):
         response = self.client.get('/')  
         self.assertTemplateUsed(response, 'home.html') 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
     
     def test_saving_and_retrieving_item(self):
+        list_ = List()
+        list_.save()  
+
         first_item = Item()
         first_item.text = "First item"
+        first_item.list = list_  
         first_item.save()
 
         second_item = Item()
         second_item.text = "Second Item"
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_objects = Item.objects.all()
         self.assertEqual(saved_objects.count(), 2)
 
         self.assertEqual(saved_objects[0].text, "First item")
+        self.assertEqual(saved_objects[0].list, list_)
         self.assertEqual(saved_objects[1].text, "Second Item") 
+        self.assertEqual(saved_objects[1].list, list_) 
 
 class ListViewTest(TestCase):
 
     def test_can_display_all_items(self):
-        Item.objects.create(text = 'item 1')
-        Item.objects.create(text = 'item 2')
+        list_ = List.objects.create()
+        Item.objects.create(text = 'item 1', list=list_)
+        Item.objects.create(text = 'item 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
