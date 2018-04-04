bc303aa64e58290425359bbe928cf35c99b9dd46
Paulo <paulo@Paulos-MacBook-Pro.local>
2018-04-03 22:05:39 -0300

    Add fabfile for automated deploys

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -35,7 +35,7 @@ def _create_or_update_dotenv():
     append('.env', 'DJANGO_DEBUG_FALSE=y')
     append('.env', f'SITENAME={env.host}')
     current_contents = run('cat .env')
-    if 'DJANGO_SECRET_KEY' not in current_commit:
+    if 'DJANGO_SECRET_KEY' not in current_contents:
         new_secret = ''.join(random.SystemRandom().choices('abcdefghijlmnopqrstuvxkwyz0123456789', k=50))
         append('.env', f'DJANGO_SECRET_KEY={new_secret}')
 
